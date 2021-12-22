9e56404238194a8876a580fc9c03058699e5f7cc
Erin Borders <borders16@gmail.com>
2021-12-22 13:38:10 -0500

    trial call successful

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import os
 from twilio.rest import Client
+from dotenv import load_dotenv
+
+# adding twilio credentials
+load_dotenv()
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+call = client.calls.create(
+    url='http://demo.twilio.com/docs/voice.xml',
+    to='+14047218853',
+    from_='+17089984068'
+)
+
