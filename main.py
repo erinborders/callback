ed517e5f48a5004b6b36d72818525963aed1e3d6
Erin Borders <borders16@gmail.com>
2021-12-22 14:48:15 -0500

    ideas for using a voicemail as the call message

--- main.py
+++ main.py
@@ -14,3 +14,14 @@ call = client.calls.create(
     from_='+17089984068'
 )
 
+# TODO: make my program message the voicemail that the caller left
+# Follow the instructions in the Twilio CLI Quickstart to install the CLI and then log in to your Twilio account.
+# Twilio CLI plugins provide additional capabilities for the CLI. The Assets Plugin allows you to manage static
+# assets in a Twilio Assets service from your command line. Install the Assets Plugin with:
+# twilio plugins:install @twilio-labs/plugin-assets
+# create a new asset service with: twilio assets:init
+# upload a file to use with your IVR: twilio assets:upload path/to/file --protected
+# Copy the URL, return to the Studio Flow and enter it into the widget configuration. Save the widget and then publish
+# the Flow. Give your number a call and you will hear your recording in the IVR instead of the text-to-speech.
+# TODO: create a program that lets me upload my voicemails so that it can then run that twilio command?
+
